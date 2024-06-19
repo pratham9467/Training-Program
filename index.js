2aa55d7c5e56e082446b6abaf2db592406735dd0
pratham9467 <76515135+pratham9467@users.noreply.github.com>
2024-06-19 19:49:34 +0530

    file

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const dropItem = document.querySelectorAll(".dropdown-item");
 const dropDown = document.querySelectorAll(".dropdown-p");
 const dropIcon = document.querySelectorAll(".drop-icon");
 
-dropItem.forEach((item) => {
+dropItem.forEach((item, index) => {
   item.addEventListener("click", () => {
-    dropDown.forEach((dropitem) => {
-      dropitem.classList.toggle("active");
-    });
-    dropIcon.forEach((icon) => {
-      icon.classList.toggle("active");
-    });
+    dropDown[index].classList.toggle("active");
+
+    dropIcon.classList[index].toggle("active");
   });
 });
-
